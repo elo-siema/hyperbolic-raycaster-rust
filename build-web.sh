db25095ecbc86851dd141f1da6d94f3609dc7f95
michal <michal.flak.96@gmail.com>
2020-10-08 21:02:13 -0400

    Wasm panic handling part 2

--- build-web.sh
+++ build-web.sh
@@ -3,3 +3,4 @@ export EMMAKEN_CFLAGS="-s USE_SDL=2"
 cargo build --release --target wasm32-unknown-emscripten
 cp target/wasm32-unknown-emscripten/release/raycaster.js html
 cp target/wasm32-unknown-emscripten/release/raycaster.wasm html
+
